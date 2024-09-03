a455ab5d5810c9d856f6aef9ed102b7ab21a5502
musyafaa <musafadila2132@gmail.com>
2024-09-03 15:04:11 +0700

    fix error

--- app/components/DashboardLayout/dashboardLayout.tsx
+++ app/components/DashboardLayout/dashboardLayout.tsx
@@ -1,9 +1,26 @@
 "use client"
-import Sidebar from '../Sidebar/sidebar';
-import Navbar from '../Navbar/navbar';
-import { LayoutProps } from '@/.next/types/app/layout';
+import dynamic from 'next/dynamic';
+import { ReactNode, useEffect, useState } from 'react';
 
-const DashboardLayout = ({ children , ...props }: LayoutProps) => {
+// Dynamically import Sidebar and Navbar with no SSR
+const Sidebar = dynamic(() => import('../Sidebar/sidebar'), { ssr: false });
+const Navbar = dynamic(() => import('../Navbar/navbar'), { ssr: false });
+
+interface DashboardLayoutProps {
+  children: ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // Ensure no mismatches by preventing rendering on the server
+  }
+
   return (
     <div className="flex">
       <Sidebar />
